7099b5da0b2601d80b26d6a2a19d016699824b8a
Hbecher <hbdu68@gmail.com>
2016-12-13 23:43:15 +0100

    Commentaires dans le sémaphore

--- src/jus/poc/prodcons/v2/Semaphore.java
+++ src/jus/poc/prodcons/v2/Semaphore.java
@@ -2,7 +2,9 @@ package jus.poc.prodcons.v2;
 
 public class Semaphore
 {
+	/** Le nombre de threads pouvant initialement passer le sémaphore */
 	private final int permits;
+	/** Le compteur du sémaphore */
 	private int c;
 
 	public Semaphore(int permits)
@@ -12,20 +14,25 @@ public class Semaphore
 
 	public synchronized void acquire() throws InterruptedException
 	{
+		// on décrémente le compteur
 		c--;
 
 		if(c < 0)
 		{
+			// s'il est négatif, c'est-à-dire qu'il n'y a plus de ressources disponibles
+			// et éventuellement des threas en attente, on se bloque
 			wait();
 		}
 	}
 
 	public synchronized void release()
 	{
+		// on incrémente le compteur
 		c++;
 
 		if(c <= 0)
 		{
+			// s'il reste des threads en attente, on en réveille un
 			notify();
 		}
 	}
